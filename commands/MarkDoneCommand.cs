e48a5f547136845c4f6128f8dfca4aa653491faa
Md Turin <matkhan01@yahoo.com>
2024-09-26 01:11:44 +0600

    mark done command done

---
+++ commands/MarkDoneCommand.cs
@@ -0,0 +1,21 @@
+﻿using Task_Tracker.attributes;
+using Task_Tracker.interfaces;
+using Task_Tracker.services;
+using TaskStatus = Task_Tracker.enums.TaskStatus;
+
+namespace Task_Tracker.commands;
+
+[ExportCommand("mark-done")]
+public class MarkDoneCommand : ICommand
+{
+    public void Execute(params string[] args)
+    {
+        if (args.Length < 2)
+            throw new KeyNotFoundException("Id is required!");
+
+        var taskId = int.Parse(args[1]);
+        var taskManager = TaskManager.Instance;
+        if (taskManager.MarkTaskStatus(TaskStatus.Done, taskId))
+            taskManager.SaveAll();
+    }
+}
